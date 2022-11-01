a52eb0d58871124f0598b09015a908c79ce99b20
NicolasFKL <inicolask@gmail.com>
2022-11-01 18:11:41 -0300

    Escenarios

---
+++ proyecto_final/Assets/Scripts/IconMission.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconMission : MonoBehaviour
+{
+    public float speedY = 0.01f;
+    public int condicion = 1;
+    public float contador = 0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Subida y Bajada
+        switch (condicion)
+        {
+            case 1:
+             transform.Translate(0,speedY,0);
+             contador = contador + 0.1f;
+             if(contador >= 5f){
+                condicion = 2;
+             }
+            break;
+
+            case 2:
+             transform.Translate(0,-speedY,0);
+             contador = contador - 0.1f;
+             if(contador <= 0f){
+                condicion = 1;
+             }
+            break;
+        }
+    }
+}
