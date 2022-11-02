a368c395477d724e294785482c8e833424eed1ac
Cristian ARiel Mamani <sr.blick@gmail.com>
2022-11-02 20:22:05 -0300

    Se completo menu solo falta agregar controles

---
+++ proyecto_final/Assets/Scripts/MenuPausaBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausaBehaviour : MonoBehaviour
+{
+    private void OnEnable() {
+        // Detiene el tiempo para que el jugador no muera cuando esta en pausa.
+        Time.timeScale = 0f;
+    }
+
+    private void OnDisable() {
+        // Vuelve activa el tiempo para seguir jugando.
+        Time.timeScale = 1f;
+    }
+    // Cambia la escena de acuerdo al parametro scene.
+    public void MainMenuButton(string scene){
+        SceneManager.LoadScene(scene);
+    }
+
+}
